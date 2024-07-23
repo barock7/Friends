fd0b21f2b5afd2a820e93848ed22406b6bc03e51
friendstech <barockandualem7@gmail.com>
2024-07-23 17:58:44 +0300

    delete

--- friends-backend/dbconfig.js
+++
@@ -1,7 +0,0 @@
-// dbConfig.js
-module.exports = {
-    host: 'localhost',
-    user: 'root',
-    password: '@eyoelbarockfitsum2024',
-    database: 'friends'
-};
--- friends-backend/testconnection.js
+++
@@ -1,8 +0,0 @@
-// testConnection.js
-const connection = require('./ dbConnection');
-
-connection.query('SELECT 1 + 1 AS solution', (err, results, fields) => {
-    if (err) throw err;
-    console.log('The solution is:', results[0].solution);
-    connection.end();
-});
